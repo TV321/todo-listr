2a23b24025a8a6e9bbc8e74968dcb2edaeb7cc0f
TV321 <tvlahovic7@gmail.com>
2019-11-30 22:42:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
 import './App.css'
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
     state = {
@@ -31,12 +33,33 @@ class App extends Component {
             return todo;
         }) });
     }
+    //Delete todo
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id
+        !== id)] });
+    }
+    //Add Todo
+    addTodo = (title) => {
+        const newTodo = {
+            id: 4,
+            title,
+            completed: false
+        }
+        this.setState({ todos: [...this.state.todos, newTodo]})
+    }
+
 
     render(){
         return (
           <div className="App">
-            <Todos todos={ this.state.todos }
-            markComplete={ this.markComplete }/>
+              <div className="container">
+                  <Header />
+                  <AddTodo addTodo={this.addTodo}  />
+                  <Todos todos={ this.state.todos }
+                      markComplete={ this.markComplete }
+                      delTodo={ this.delTodo }
+                  />
+              </div>
           </div>
         );
     }
